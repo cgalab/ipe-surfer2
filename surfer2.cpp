f461f398439d2458692bc285d4f35844a0064cf3
Günther Eder <geder@cs.sbg.ac.at>
2019-12-13 10:07:12 +0100

    Makefile

---
+++ surfer2.cpp
@@ -0,0 +1,60 @@
+// --------------------------------------------------------------------
+// Ipelet for creating regular k-gons
+// --------------------------------------------------------------------
+#include <ipelet.h>
+#include <ipepath.h>
+#include <ipepage.h>
+using namespace ipe;
+// --------------------------------------------------------------------
+class KGonIpelet : public Ipelet {
+public:
+	virtual int ipelibVersion() const { return IPELIB_VERSION; }
+	virtual bool run(int, IpeletData *data, IpeletHelper *helper);
+};
+// --------------------------------------------------------------------
+bool KGonIpelet::run(int, IpeletData *data, IpeletHelper *helper) {
+	Page *page = data->iPage;
+	int sel = page->primarySelection();
+	if (sel < 0) {
+		helper->message("No selection");
+		return false;
+	}
+	const Path *p = page->object(sel)->asPath();
+	if (p == 0 || p->shape().countSubPaths() != 1 ||
+			p->shape().subPath(0)->type() != SubPath::EEllipse) {
+		helper->message("Primary selection is not a circle");
+		return false;
+	}
+	String str = helper->getParameter("n");   // get default value from Lua wrapper
+	if (!helper->getString("Enter k (number of corners)", str))
+		return false;
+	int k = Lex(str).getInt();
+	if (k < 3 || k > 1000)
+		return false;
+	const Ellipse *e = p->shape().subPath(0)->asEllipse();
+	Matrix m = p->matrix() * e->matrix();
+
+	Vector center = m.translation();
+	Vector v = m * Vector(1,0);
+	double radius = (v - center).len();
+	Curve *sp = new Curve;
+	double alpha = 2.0 * IpePi / k;
+	Vector v0 = center + radius * Vector(1,0);
+	for (int i = 1; i < k; ++i) {
+		Vector v1 = center + radius * Vector(Angle(i * alpha));
+		sp->appendSegment(v0, v1);
+		v0 = v1;
+	}
+	sp->setClosed(true);
+	Shape shape;
+	shape.appendSubPath(sp);
+	Path *obj = new Path(data->iAttributes, shape);
+	page->append(ESecondarySelected, data->iLayer, obj);
+	helper->message("Created regular k-gon");
+	return true;
+}
+// --------------------------------------------------------------------
+IPELET_DECLARE Ipelet *newIpelet() {
+	return new KGonIpelet;
+}
+// --------------------------------------------------------------------
---
+++ surfer2.lua
@@ -0,0 +1,60 @@
+-- surfer2.lua is a wrapper for surfer2 to be used in Ipe.
+--
+-- Copyright 2019 Günther Eder - geder@cs.sbg.ac.at
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+label = "Surfer2"
+
+about = [[ Create the (weighted) straight skeleton of a PSLG. ]]
+
+ipelet = false
+
+function collect_input(model)
+   local p = model:page()
+   local newPage  = ipe.Page()
+   local newLayer = newPage:addLayer("input")
+   for i, obj, sel, layer in p:objects() do
+      if sel then
+         newPage:insert(nil,obj,nil,newLayer)
+	   end	    
+   end
+   newPage:removeLayer("alpha")
+   newPage:setVisible(1, "input", true)
+   return newPage
+end
+
+function create_skeleton(model)
+   page = collect_input(model)
+   doc = ipe.Document()
+   doc:insert(1,page)
+   doc:save(".surfer2-ipelet.ipe", nil, nil)
+end
+
+-- parameters for the C++ code
+parameters = { n = "7" }
+
+
+function c_test(model)
+  if not ipelet then ipelet = assert(ipe.Ipelet("surfer2")) end
+  model:runIpelet(label, ipelet, 1, parameters)
+end
+
+methods = {
+  { label="Create Skeleton", run = create_skeleton },
+  { label="Run C", run = c_test},
+}
+
+-- define a shortcut for this function
+--shortcuts.ipelet_1_skeleton = "Shift+H"
