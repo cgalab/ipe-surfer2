c5160968c6678994e907fea39a63326b28a34656
Günther Eder <geder@cs.sbg.ac.at>
2019-12-16 13:37:28 +0100

    cont.

--- ipelet/inc/ipesurfer2.h
+++ ipelet/inc/ipesurfer2.h
@@ -4,6 +4,8 @@
 #include <ipepath.h>
 #include <ipepage.h>
 
+#include "BasicInput.h"
+
 using namespace ipe;
 
 IPELET_DECLARE Ipelet *newIpelet();
@@ -18,3 +20,21 @@ IPELET_DECLARE Ipelet *newIpelet() {
 	return new KGonIpelet;
 }
 
+class ISBasicInput : public BasicInput {
+public:
+	ISBasicInput(
+			std::vector<std::pair<double,double>> p_points,
+			std::vector<std::pair<int,int>> m_edges,
+			std::vector<double> m_edgeWeights,
+			std::vector<unsigned> m_degree
+	) {
+		for(size_t i = 0; i < p_points.size(); ++i) {
+			auto p = p_points[i];
+			add_vertex(Vertex(Point_2(p.first,p.second),m_degree[i],i));
+		}
+		for(size_t i = 0; i < m_edges.size(); ++i) {
+			auto e = m_edges[i];
+			add_edge(e.first,e.second,m_edgeWeights[i]);
+		}
+	}
+};
--- ipelet/src/ipesurfer2.cpp
+++ ipelet/src/ipesurfer2.cpp
@@ -3,6 +3,12 @@
 #include <sstream>
 #include <string>
 
+#include <vector>
+#include <unordered_set>
+#include <boost/functional/hash.hpp>
+
+#include "SkeletonStructure.h"
+
 #include "ipesurfer2.h"
 
 bool KGonIpelet::run(int, IpeletData *data, IpeletHelper *helper) {
@@ -16,7 +22,7 @@ bool KGonIpelet::run(int, IpeletData *data, IpeletHelper *helper) {
 	/* get selected paths from page */
 	std::vector<const Path*> selection;
 	for(size_t i = 0; i < page->count(); ++i) {
-		if(page->select(i) > 0) {
+		if(page->select(i) > 0 && page->object(i)->type() == Object::Type::EPath) {
 			selection.emplace_back(page->object(i)->asPath());
 		}
 	}
@@ -25,20 +31,88 @@ bool KGonIpelet::run(int, IpeletData *data, IpeletHelper *helper) {
 //	helper->message(ss.str().c_str());
 //	ss.clear();
 
-	std::vector<CurveSegment> segments;
+	std::vector<std::tuple<Vector,Vector,double>> vertexPairs;
+
+	std::vector<std::pair<CurveSegment,Attribute>> segments;
 	/* get segments from selection */
 	for(auto p : selection) {
-		if(p->shape().isSegment()) {
-			for(size_t i = 0; i < p->shape().subPath(0)->asCurve()->countSegments(); ++i) {
-				segments.emplace_back(p->shape().subPath(0)->asCurve()->segment(i));
+		for(int sub = 0; sub < p->shape().countSubPaths(); ++sub) {
+
+			if(p->shape().subPath(sub)->type() == SubPath::Type::ECurve) {
+				for(int i = 0; i < p->shape().subPath(sub)->asCurve()->countSegments(); ++i) {
+					segments.push_back({p->shape().subPath(sub)->asCurve()->segment(i),p->pen()});
+				}
+				if(p->shape().subPath(sub)->asCurve()->closed()) {
+					auto firstSeg = p->shape().subPath(sub)->asCurve()->segment(0);
+					auto lastSeg = p->shape().subPath(sub)->asCurve()->segment(p->shape().subPath(sub)->asCurve()->countSegments()-1);
+
+					Vector vA = firstSeg.cp(0); Vector vB = lastSeg.last();
+
+					if(p->pen().isNumber()) {
+						vertexPairs.push_back({vA,vB,p->pen().number().toDouble()});
+					} else {
+						vertexPairs.push_back({vA,vB,1.0});
+					}
+				}
+			}
+
+		}
+	}
+
+	std::unordered_map<std::pair<double, double>, int, boost::hash<std::pair<double, double>>> pointsMap;
+
+	for(auto s : segments) {
+		if(s.first.type() == CurveSegment::Type::EArc) {
+			std::cerr << "TODO: an EArc not added!" << std::endl;
+		} else if(s.first.type() == CurveSegment::Type::ESegment) {
+			std::cerr << "added ESegment" << std::endl;
+			Vector vA = s.first.cp(0); Vector vB = s.first.last();
+			pointsMap.insert({{vA.x,vA.y},-1});
+			pointsMap.insert({{vB.x,vB.y},-1});
+
+			if(s.second.isNumber()) {
+				vertexPairs.push_back({vA,vB,s.second.number().toDouble()});
+			} else {
+				vertexPairs.push_back({vA,vB,1.0});
 			}
 		}
 	}
 
-	for(auto s : segments) {
-		auto Ps = s.arc().beginp();
-		auto Pe = s.arc().endp();
+	std::vector<std::pair<double,double>> points;
+
+	for(auto& p : pointsMap) {
+		p.second = points.size();
+		points.push_back(p.first);
 	}
+
+	std::vector<std::pair<int,int>> edges;
+	std::vector<double> edgeWeights;
+	std::vector<unsigned> vertexDegree(points.size(),0);
+
+
+	for(auto& edge : vertexPairs) {
+		auto itA = pointsMap.find({std::get<0>(edge).x,std::get<0>(edge).y});
+		auto itB = pointsMap.find({std::get<1>(edge).x,std::get<1>(edge).y});
+		edges.push_back({itA->second,itB->second});
+		edgeWeights.push_back(std::get<2>(edge));
+		++vertexDegree[itA->second];
+		++vertexDegree[itB->second];
+	}
+
+
+	ISBasicInput input(points,edges,edgeWeights,vertexDegree);
+
+	SkeletonStructure s(input);
+
+	s.initialize(-1);
+	s.wp.advance_to_end();
+
+
+//	  if (write_ipe) {
+//	      s.get_skeleton().write_ipe(os, skoffset);
+//	    } else {
+//	      s.get_skeleton().write_obj(os);
+//	    }
 
 
 //	const Path *p = page->object(sel)->asPath();
